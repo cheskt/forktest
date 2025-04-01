f8ad95af2885fefec57d2ce263580ea9f2b9612d
caffrose415 <super_vet@naver.com>
2025-04-01 10:19:09 +0900

    html first commit

---
+++ html/tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config ={
+    theme:{
+        extend:{
+            colors:{
+                clifford:"#da373d",
+                carrot:"#e67e22",
+                mycolor:"#8e44ad",
+                mycolorg:"#2ecc71",
+                danger:"#e74c3c"
+            }
+        }
+    }
+}
